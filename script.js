41541e227fe109fbdfc1a9039a0395a9442b74f9
AbdulBaseer <160875997+AbdulBaseer1939@users.noreply.github.com>
2024-11-26 23:01:53 +0500

    HTML and JS file has added and 2 problems are done

---
+++ script.js
@@ -0,0 +1,7 @@
+// Problem#1: Write a script to greet your website visitor using JS alert box. 
+
+alert("Hello, Welcome to my website!");
+
+// Problem#2: Write a script to display following message on your web page:
+
+alert("Error! Please enter valid password");
