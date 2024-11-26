1fc61006fa04023317a2a9d8ef2981d9a2a45022
AbdulBaseer <160875997+AbdulBaseer1939@users.noreply.github.com>
2024-11-26 23:18:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 
 alert("Hello, Welcome to my website!");
 
-// Problem#2: Write a script to display following message on your web page:
+// Problem#2: Write a script to display the message "Error! Please enter valid password" on your web page:
 
 alert("Error! Please enter valid password");
+
+// Problem#3: Write a script to display the message "Welcome to JS Land... Happy Coding!" on your web, page: (Hint : Use line break).
+
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Problem#4:  Write a script to display following messages in sequence:
+
+// Display the first alert
+alert("Welcome to JS Land...");
+
+// Display the second alert with the checkbox option
+alert("Happy Coding!\n\nPrevent this page from creating additional dialogs.");
