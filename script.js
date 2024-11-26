4e6309082a03d3cc6f026cc546f25ca5d60ba76a
AbdulBaseer <160875997+AbdulBaseer1939@users.noreply.github.com>
2024-11-26 23:28:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 // Problem#1: Write a script to greet your website visitor using JS alert box. 
-
 alert("Hello, Welcome to my website!");
 
 // Problem#2: Write a script to display the message "Error! Please enter valid password" on your web page:
-
 alert("Error! Please enter valid password");
 
 // Problem#3: Write a script to display the message "Welcome to JS Land... Happy Coding!" on your web, page: (Hint : Use line break).
-
 alert("Welcome to JS Land...\nHappy Coding!");
 
 // Problem#4:  Write a script to display following messages in sequence:
-
 // Display the first alert
 alert("Welcome to JS Land...");
-
 // Display the second alert with the checkbox option
 alert("Happy Coding!\n\nPrevent this page from creating additional dialogs.");
+
+// Problem#5:  Generate the message "Hello... I can run JS hrough my web browser's console" through browser’s developer console: 
+console.log("Hello... I can run JS hrough my web browser's console");
+
+// Problem#6: Make use of alerts in your new/existing HTML & CSS project.
+function showAlert() {
+    alert("This is an alert message!");
+}
